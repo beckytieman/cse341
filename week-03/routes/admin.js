0372aa3616bd7a16afa8e3fddfd26efee6c5eab1
beckytieman45 <beckytieman@gmail.com>
2022-01-19 18:35:52 -0700

    fix mobile view

--- week-03/routes/admin.js
+++ week-03/routes/admin.js
@@ -2,14 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// /admin/add-product
-router.get('/add-product', productsController.getAddProduct);
+// /admin/add-product (filtered from app.js)
+router.get('/add-product', adminController.getAddProduct);
 
 // /admin/add-product
-router.post('/add-product', productsController.postAddProduct);
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
