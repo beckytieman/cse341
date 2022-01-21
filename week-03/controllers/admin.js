919429f9c0908b21b799ea05ac13d5530202e431
beckytieman45 <beckytieman@gmail.com>
2022-01-21 10:38:53 -0700

    finish week03 work

--- week-03/controllers/admin.js
+++ week-03/controllers/admin.js
@@ -58,3 +58,9 @@ exports.getAddProduct = (req, res, next) => {
         });
       });
   };
+
+  exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.deleteById(prodId);
+    res.redirect('/admin/products');
+  };
