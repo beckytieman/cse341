d0f0a0055b6facf5d24413cc62fb10084362daaf
beckytieman45 <beckytieman@gmail.com>
2022-01-28 14:42:21 -0700

    second try

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 const path = require('path');
+const PATH = process.env.PORT || 3000;
+
+const cors = require('cors');
+
+const corsOptions = {
+  origin: "https://cse341-btieman.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://BTieman:wVMtYy1r23ZMxURA@cluster0.xmdot.mongodb.net/shop?retryWrites=true&w=majority";
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -32,23 +51,45 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// mongoose
+//   .connect('mongodb+srv://BTieman:wVMtYy1r23ZMxURA@cluster0.xmdot.mongodb.net/shop?retryWrites=true&w=majority')
+//   .then(result => {
+//     User.findOne().then(user => {
+//       if (!user) {
+//         const user = new User({
+//         name: 'Becky',
+//         email: 'becky@test.com',
+//         cart: {
+//           items: []
+//         }
+//       });
+//     user.save();
+//       }
+//     });
+//     app.listen(3000);
+//   })
+//   .catch(err => {
+//     console.log(err);
+
 mongoose
-  .connect('mongodb+srv://BTieman:wVMtYy1r23ZMxURA@cluster0.xmdot.mongodb.net/shop?retryWrites=true&w=majority')
+  .connect(
+    MONGODB_URL, options
+  )
   .then(result => {
     User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-        name: 'Becky',
-        email: 'becky@test.com',
-        cart: {
-          items: []
-        }
-      });
-    user.save();
-      }
-    });
-    app.listen(3000);
+            if (!user) {
+              const user = new User({
+              name: 'Becky',
+              email: 'becky@test.com',
+              cart: {
+                items: []
+              }
+            });
+          user.save();
+            }
+          });
+    app.listen(PORT);
   })
   .catch(err => {
     console.log(err);
-});
+  });
