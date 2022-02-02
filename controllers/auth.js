316c68871161ccb7f50b912e7aa290aa753737c1
beckytieman45 <beckytieman@gmail.com>
2022-02-02 16:03:51 -0700

    sendgrid edits

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+// const transporter = nodemailer.createTransport(sendgridTransport({
+//     auth: {
+//         api_key: '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'
+//     }
+// }));
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
@@ -82,8 +90,17 @@ exports.postSignup = (req, res, next) => {
         return user.save();
         })
         .then(result => {
-        res.redirect('/login');
-        });
+            res.redirect('/login');
+            // return transporter.sendMail({
+            //     to: email,
+            //     from: 'shop@node-complete.com',
+            //     subject: 'Signup succeeded!',
+            //     html: '<h1>You successfully signed up!</h1>'
+            // });
+        })
+        .catch(err => {
+            console.log(err);
+        })
     })  
     .catch(err => {
         console.log(err);
