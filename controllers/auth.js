419057495ca5d330ba0fd81833a13b2a8bc5f3b8
beckytieman45 <beckytieman@gmail.com>
2022-02-01 20:09:20 -0700

    week05: sessions and cookies

---
+++ controllers/auth.js
@@ -0,0 +1,19 @@
+exports.getLogin = (req, res, next) => {
+    // const isLoggedIn = req
+    // .get('Cookie')
+    // .split(';')[0]
+    // .trim()
+    // .split('=')[0];
+    
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: false
+      });
+  };
+
+exports.postLogin = (req, res, next) => {
+    req.session.isLoggedIn = true;
+    res.redirect('/');
+  };
+  
