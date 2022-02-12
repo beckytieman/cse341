8519332ef2006f1e239b40fc772f27da0746d857
beckytieman45 <beckytieman@gmail.com>
2022-02-12 09:51:59 -0700

    fix error handling

--- app.js
+++ app.js
@@ -91,9 +91,9 @@ app.get('/500,', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, reg, res, next) => {
-  // res.redirect('/500');
-  res.status(500).render('500', { pageTitle: 'Error!', path: '/500',
-  isAuthenticated: req.session.isLoggedIn });
+   res.redirect('/500');
+  // res.status(500).render('500', { pageTitle: 'Error!', path: '/500',
+  // isAuthenticated: req.session.isLoggedIn });
 });
 
 app.use(cors(corsOptions));
